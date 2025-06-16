7e45de6c3443437012d75d87a645de1e7e9b606f
Errett <errett.bentz@gmail.com>
2025-06-16 13:21:02 -0500

    added user context to system prompt and the start of allowing the agent to use functions by defining a function schema

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ from google import genai
 
 args_len = len(sys.argv)
 verbose = False
-system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 model_name = 'gemini-2.0-flash-001'
 
 if args_len > 1:
@@ -17,15 +25,40 @@ if args_len > 1:
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     user_prompt = sys.argv[1]
+    schema_get_files_info = genai.types.FunctionDeclaration(
+        name="get_files_info",
+        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+        parameters=genai.types.Schema(
+            type=genai.types.Type.OBJECT,
+            properties={
+                "directory": genai.types.Schema(
+                    type=genai.types.Type.STRING,
+                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
+    )
+    available_functions = genai.types.Tool(
+        function_declarations = [
+            schema_get_files_info,
+        ],
+    )
     messages = [
-    genai.types.Content(role="user", parts=[genai.types.Part(text=user_prompt)]),
+        genai.types.Content(role="user", parts=[genai.types.Part(text=user_prompt)]),
     ]
     response = client.models.generate_content(
         model=model_name,
         contents=messages,
-        config=genai.types.GenerateContentConfig(system_instruction=system_prompt)
+        config=genai.types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt,
+        )
     )
-    print(f"Response Text:\n\t {response.text}")
+    if response.function_calls:
+        for call in response.function_calls:
+            print(f"Calling function: {call.name}({call.args})")
+        if response.code_execution_result: print(f"Code execution response: {response.code_execution_result}")
+    if response.text: print(f"Response Text:\n\t {response.text}")
     if verbose:
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
